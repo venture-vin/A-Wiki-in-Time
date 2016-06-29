c443d28f12507eb2ef9679f0ff8441c744ce34d1
Olya Royall <olya.vgrig+git@gmail.com>
2016-06-29 11:04:14 -0700

    add configuration to set up for production build

---
+++ webpack.prod.config.js
@@ -0,0 +1,53 @@
+const path = require('path')
+const webpack = require('webpack')
+
+module.exports = {
+  devtool: 'source-map',
+
+  entry: [
+    './app/index'
+  ],
+
+  output: {
+    path: path.join(__dirname, 'public'),
+    filename: 'bundle.js',
+    publicPath: '/public/'
+  },
+
+  plugins: [
+    new webpack.optimize.UglifyJsPlugin({
+      minimize: true,
+      compress: {
+        warnings: false
+      }
+    }),
+    new webpack.DefinePlugin({
+      'process.env': {
+        'NODE_ENV': JSON.stringify('production')
+      }
+    })
+  ],
+
+  module: {
+    loaders: [
+      { test: /\.jsx?$/,
+        loader: 'babel-loader',
+        exclude: /node_modules/ },
+        { test: /\.css?$/,
+          loader: 'style-loader!css-loader',
+          include: path.join(__dirname, 'app', 'styles') },
+          {
+            test: /.*\.(gif|png|jpe?g|svg)$/i,
+            loaders: [
+              'file?hash=sha512&digest=hex&name=[hash].[ext]',
+              'image-webpack'
+            ]
+          },
+          { test: /\.(ttf|eot|svg)(\?v=[0-9]\.[0-9]\.[0-9])?$/, loaders: [
+            'file?name=/fonts/endor/[name].[ext]',
+            "file-loader"  ]  }
+          ]
+        },
+      ]
+    }
+  }
