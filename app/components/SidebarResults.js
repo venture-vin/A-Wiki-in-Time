b15c2229e11da27e2ad344c93e614ca54a473aa2
Olya Royall <email": null>
2016-06-30 16:14:33 -0700

    fix margins'

--- app/components/SidebarResults.js
+++ app/components/SidebarResults.js
@@ -8,8 +8,8 @@ var SidebarResults = React.createClass({
               return (
                 <li key={event.qID} className="card-info">
                   <div className="card card-block">
-                    <h3 className="card-title">{event.title}</h3>
-                    <span>Coordinates: {event.longitude} : {event.latitude} </span>
+                    <h4 className="card-title">{event.title}</h4>
+                    <span> Latitude: {(event.latitude).toFixed(2)} -- Longitude: {(event.longitude).toFixed(2)} </span>
                     <br/>
                     <a href={event.event_url} target="_blank">Read More Here</a>
                     <p className="card-text">{event.description}</p>
