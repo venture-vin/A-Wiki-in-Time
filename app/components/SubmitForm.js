872005c63617b1028392706e6c89ea9402609669
Olya Royall <email": null>
2016-06-30 09:20:06 -0700

    Add stylings for the button

--- app/components/SubmitForm.js
+++ app/components/SubmitForm.js
@@ -27,8 +27,10 @@ function SubmitForm(props){
               <input id='polygon-input' type='hidden' name='polygon' value='' />
               <input id='lat-input' type='hidden' name='lat' value='' />
               <input id='long-input' type='hidden' name='long' value='' />
-              <button id='polygon-mode' className="btn btn-block">POLYGONMODE</button>
-              <button id='reset-button' className="btn btn-block">RESET POLYGON</button>
+                <div className="form-group col-sm-4 col-sm-offset-4">
+                  <button id='polygon-mode' className="btn btn-block btn-info">POLYGONMODE</button>
+                  <button id='reset-button' className="btn btn-block btn-info">RESET POLYGON</button>
+                </div>
             </div>
             <div className="form-group col-sm-4 col-sm-offset-4">
               <input
