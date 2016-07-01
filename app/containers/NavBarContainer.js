b45944066321db2c4668a7684a27f689b557c786
Olya Royall <email": null>
2016-06-30 19:32:15 -0700

    update instructions content and button

--- app/containers/NavBarContainer.js
+++ app/containers/NavBarContainer.js
@@ -15,14 +15,14 @@ var NavBarContainer = React.createClass({
   render(){
     return(
       <nav className="navbar">
-        {/*<div className="container">*/}
           <div id='header'>
             {React.cloneElement(this.props.children)}
+            <div id="title">
             <span className='header-div'><a href='/'>A </a></span>
             <span className='header-div' id='wiki-header'><a href='/'>Wiki</a></span>
             <span className='header-div'><a href='/'>in </a></span>
             <span className='header-div'><a href='/'>Time</a></span>
-          {/*</div>*/}
+            </div>
         </div>
       </nav>
     )
