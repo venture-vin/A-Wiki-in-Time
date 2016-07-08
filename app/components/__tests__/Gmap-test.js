8c70de3f35bec5e1302728fda51e9ae137b8a5e2
Raymond Sapida <ray.sapida@gmail.com>
2016-07-07 18:06:49 -0700

    Create fake objects for google and jquery for unit testing

--- app/components/__tests__/Gmap-test.js
+++ app/components/__tests__/Gmap-test.js
@@ -1,22 +1,60 @@
-// jest.unmock('../app/components/Gmap');
-//
-// import React from 'react';
-// import ReactDOM from 'react-dom';
-// import TestUtils from 'react-addons-test-utils';
-// import Gmap from '../app/components/Gmap';
-//
-// describe('Gmap', () => {
-//   it('renders the component', () => {
-//     let initialCenter = {lat: 37.784580, lng: -122.397437};
-//     let google = {};
-//     // <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyCj9yUP6BgnHAX-qFkkEQDmgce9hB_vpuo"></script>
-//     const gmap = TestUtils.renderIntoDocument(
-//       <Gmap initialCenter={initialCenter} ref="map" onUpdate={() => {}}/>
-//     );
-//
-//     const gmapNode = ReactDOM.findDOMNode(gmap);
-//
-//     expect(gmapNode.textContent).toEqual('Off');
-//
-//   });
-// });
+jest.unmock('../Gmap');
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+import TestUtils from 'react-addons-test-utils';
+import Gmap from '../Gmap';
+
+describe('Gmap', () => {
+  it('renders the component', () => {
+    let initialCenter = {lat: 37.784580, lng: -122.397437};
+
+    window.$ = function() {
+      return {
+        on: function(){}
+      }
+    }
+    window.google = {
+      maps: {
+        Geocoder: function() {},
+        GeocoderLocationType: {},
+        GeocoderStatus: {},
+        InfoWindow: function() {},
+        LatLng: function() {},
+        LatLngBounds: function() {},
+        Map: function () {
+          return {
+            setTilt: function () { },
+            mapTypes: {
+              set: function () { }
+            },
+            overlayMapTypes: {
+              insertAt: function () { },
+              removeAt: function () { }
+            }
+          };
+        },
+        Marker: function() {},
+        MarkerImage: function() {},
+        Point: function() {},
+        Polygon: function() {
+          return {
+            setMap: function() {}
+          }
+        },
+        event: {
+          addListener: function () { }
+        }
+      }
+    };
+
+    const gmap = TestUtils.renderIntoDocument(
+      <Gmap initialCenter={initialCenter} onUpdate={() => {}}/>
+    );
+
+    const gmapNode = ReactDOM.findDOMNode(gmap);
+
+    expect(gmapNode.textContent).toEqual('Loading map ...');
+
+  });
+});
