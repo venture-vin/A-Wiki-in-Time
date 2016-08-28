fa657f405d6695931bc479a43adff5282d9b622e
Ryan Glassett <rglassett@gmail.com>
2016-08-28 11:15:07 -0700

    Add alt for Events
    The logic for handling successful fetch was copied directly from the
    SearchContainer. It needs a little cleaning up, which will happen in the
    next commit.

---
+++ app/actions/EventActions.js
@@ -0,0 +1,6 @@
+import alt from '../shared/alt'
+
+export default alt.generateActions(
+  'fetchEventsCompleted',
+  'fetchEventsFailed',
+)
---
+++ app/sources/EventSource.js
@@ -0,0 +1,17 @@
+import axios from 'axios'
+
+import EventActions from '../actions/EventActions'
+
+export default EventSource = {
+  fetchEvents: {
+    remote: function (state, data) {
+      return axios({
+        method: 'post',
+        url: 'https://a-wiki-in-time-backend.herokuapp.com/query',
+        data: data
+      })
+    },
+    success: EventActions.fetchEventsCompleted,
+    error: EventActions.fetchEventsFailed,
+  }
+}
